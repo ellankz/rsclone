96e2688cc06e3ff3e7975345039eb35c3c4765aa
VVladislaVLL <vlad2002kochurko@gmail.com>
2021-01-21 01:11:43 +0300

    fix: modal window position

--- src/models/StartLevelView.ts
+++ src/models/StartLevelView.ts
@@ -20,6 +20,8 @@ export class StartLevelView {
 
   private zombiesTypes: Set<ZombieType>;
 
+  private running: boolean;
+
   constructor(engine: Engine, startLevel: () => void, types: Array<string>) {
     this.engine = engine;
     this.startLevel = startLevel;
@@ -66,11 +68,11 @@ export class StartLevelView {
 
   private init(): void {
     this.engine.start('scene');
-    let running = false;
+    this.running = false;
 
     const viewAnimation = (node: any) => {
       const { view } = node.layer;
-      running = true;
+      this.running = true;
 
       const copyNode: any = node;
       copyNode.update = () => {
@@ -84,7 +86,7 @@ export class StartLevelView {
                   if (view.position.x <= 0) {
                     delete copyNode.update;
                     view.position.x = 0;
-                    running = false;
+                    this.running = false;
                   }
                   view.move(this.engine.vector(-2.5, 0));
                 }
@@ -104,7 +106,7 @@ export class StartLevelView {
     viewAnimation(this.zombies[0]);
 
     this.zombies[1].update = () => {
-      if (running === false) {
+      if (this.running === false) {
         this.zombies.forEach((zombie) => {
           zombie.destroy();
         });
